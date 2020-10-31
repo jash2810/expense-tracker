de5bf6d9ab9164b2458277251de3c820b3dcbcb2
jash2810 <jashdetroj@gmail.com>
2020-10-31 22:30:10 +0530

    user model updated

--- expense-tracker.api/models/user.js
+++ expense-tracker.api/models/user.js
@@ -9,6 +9,27 @@ const UserSchema = new mongoose.Schema({
     cred: {
         email: {type: String, required: true, unique: true},
         password: {type: String, required: true}
+    },
+    account: {
+        credited: [{
+            month: {type: Number, required: false},
+            year: {type: Number, required: false},
+            description: {type: String, required: true},
+            date: {type: Date, default: Date.now},
+            source: {type: String, required: false},
+            amount: {type: Number},
+            dueDate: {type: Date, required: false}
+        }],
+        debited: [{
+            month: {type: Number, required: false},
+            year: {type: Number, required: false},
+            description: {type: String, required: true},
+            date: {type: Date, default: Date.now},
+            category: {type: String, default: 'none', required: true},
+            toPerson: {type: String, required: false},
+            amount: {type: Number, required: true},
+            dueDate: {type: Date, required: false}
+        }]
     }
 }, {
     timestamps: true
