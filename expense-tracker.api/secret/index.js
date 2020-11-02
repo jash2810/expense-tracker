70902c7c19baaa23be6bd25aa24c4aab4986a9ac
jash2810 <jashdetroj@gmail.com>
2020-11-02 15:16:41 +0530

    online database MLAB set successfully

--- expense-tracker.api/secret/index.js
+++ expense-tracker.api/secret/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     mongodb: {
-        localURI: 'mongodb://localhost:27017/expensetracker'
+        localURI: 'mongodb://localhost:27017/expensetracker',
+        onlineURI: 'mongodb://expensetracker:et2020@ds235768.mlab.com:35768/expensetracker'
     }
 }
