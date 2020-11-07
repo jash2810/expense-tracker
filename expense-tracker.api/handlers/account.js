4ca0094a4f3173cda38f3f9320f1495faffef4a4
unknown <p.hirva98@gmail.com>
2020-11-07 11:03:04 +0530

    filtering done

--- expense-tracker.api/handlers/account.js
+++ expense-tracker.api/handlers/account.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const seeds = require('../seeds/user')
 
 const { deleteOne } = require('../models/user')
+const { credited } = require('../seeds/user')
 
 exports.credit = async (req, res, next) => {
     try {
@@ -150,24 +151,24 @@ exports.seedDebited = async (req, res, next) => {
 exports.filter = async (req, res, next) => {
     try {
         var userId = req.params.id 
-        var {start,end} = req.body
+        var start = req.body.start
+        var end = req.body.end
 
-        var startDate= new Date(start).toISOString()
-        var endDate= new Date(end).toISOString()
-        console.log(startDate)
-        console.log(endDate)
-        
-        var user = await db.User.find({
-            "_id": userId,
-            "account.credited": { 
-              "$elemMatch": {  date:{"$gte": start,"$lt": end}  }
-           }
+        var startDate= start.split('-')
+        var startNewDate = new Date(parseInt(startDate[0]),parseInt(startDate[1])-1,parseInt(startDate[2])+1)
        
-         } )//.then(doc =>{
-            
-             res.json({data:user, msg: 'user found', success: true})
-        // })
-         console.log(user)
+        var endDate= end.split('-')
+        var endNewDate = new Date(parseInt(endDate[0]),parseInt(endDate[1])-1,parseInt(endDate[2])+1)
+
+        var user = await db.User.findById(userId,{account: 1,details: 1} )
+        var credited=[],debited=[]
+
+        credited = user.account.credited.filter(c =>{return(c.date > startNewDate && c.date < endNewDate)})
+        debited = user.account.debited.filter(d =>{return(d.date > startNewDate && d.date < endNewDate)})
+
+         res.json({credited:credited,debited:debited, msg: 'user found', success: true})
+      
+        
     } catch (error) {
         error.status = 400
         console.log(error);        
